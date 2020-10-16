ee2e4b291e991d8a827dd541634cff1dfd1bcf07
Aneesh vijayachandran <aneeshv28@gmail.com>
2020-10-16 21:41:22 +0530

    Updates

---
+++ precache-manifest.f0bd015e22020c5c0b071ce4f3a6ad0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98970525de71bce3ddd53f52dc84e8d6",
+    "url": "/inspire01/index.html"
+  },
+  {
+    "revision": "064a2da11d1466401443",
+    "url": "/inspire01/static/css/main.8cfbf0d6.chunk.css"
+  },
+  {
+    "revision": "480f7bae7ec578fed988",
+    "url": "/inspire01/static/js/2.91d0cf36.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/inspire01/static/js/2.91d0cf36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "064a2da11d1466401443",
+    "url": "/inspire01/static/js/main.1ccd5875.chunk.js"
+  },
+  {
+    "revision": "da43ac9e3721d565cf65",
+    "url": "/inspire01/static/js/runtime-main.6dd45684.js"
+  }
+]);
